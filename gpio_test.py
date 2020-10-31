205c8eea656a63740656eda671efd5b2ed1ae029
Alyssa Margalit <alyssam0@usc.edu>
2020-10-30 18:27:08 -0700

    s

--- gpio_test.py
+++ gpio_test.py
@@ -1,10 +1,30 @@
-from gpiozero import LED
-from time import sleep
+import RPi.GPIO as GPIO
+import time
 
-led = LED(17)
+servoPIN = 17
+GPIO.setmode(GPIO.BCM)
+GPIO.setup(servoPIN, GPIO.OUT)
 
-while True:
-	led.on()
-	sleep(1)
-	led.off()
-	sleep(1)
+p = GPIO.PWM(servoPIN, 50) # GPIO 17 for PWM with 50Hz
+p.start(2.5) # Initialization
+try:
+  while True:
+    p.ChangeDutyCycle(5)
+    time.sleep(0.5)
+    p.ChangeDutyCycle(7.5)
+    time.sleep(0.5)
+    p.ChangeDutyCycle(10)
+    time.sleep(0.5)
+    p.ChangeDutyCycle(12.5)
+    time.sleep(0.5)
+    p.ChangeDutyCycle(10)
+    time.sleep(0.5)
+    p.ChangeDutyCycle(7.5)
+    time.sleep(0.5)
+    p.ChangeDutyCycle(5)
+    time.sleep(0.5)
+    p.ChangeDutyCycle(2.5)
+    time.sleep(0.5)
+except KeyboardInterrupt:
+  p.stop()
+  GPIO.cleanup()
