54d3025067633a7e3e42534b1fcff698666f1280
Alyssa Margalit <alyssam0@usc.edu>
2020-10-30 23:21:42 -0700

    s

--- gpio_test.py
+++ gpio_test.py
@@ -4,7 +4,7 @@ GPIO.setwarnings(False)
 servoPIN = 17
 GPIO.setmode(GPIO.BCM)
 GPIO.setup(servoPIN, GPIO.OUT)
-GPIO.setup(27,GPIO.OUT)
+GPIO.setup(27,GPIO.IN)
 
 p = GPIO.PWM(servoPIN, 50) # GPIO 17 for PWM with 50Hz
 p.start(2.5) # Initialization
